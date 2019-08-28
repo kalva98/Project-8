dcabb112298ba2c30d4b741c7734eacbc229ae84
kalva98 <51241291+kalva98@users.noreply.github.com>
2019-08-28 09:57:24 -0400

    Final Project

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require('express'); //look up require express
 const app = express(); // look up 
 const bodyParser = require('body-parser');
-// const pug = require('pug');
-
-//--------------------------------------------------
-//words to look up 
-//bodyparser, extends layout
 
 const routes = require('./routes/index');
 const books = require('./routes/books');
 
-// Database
-
-// const db = require('./config/database');
-
-// Test DB
-// db.authenticate()
-//   .then(() => console.log('Database connected'))
-//   .catch(err => console.log('Error: ' + err))
-
-
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
@@ -34,12 +17,6 @@ app.use('/', routes);
 //routing to homepage
 app.use('/books', books);
 //redirecting to /books page without using the word redirect
-
-// app.use(function(req, res, next){
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
 
 const PORT = process.env.PORT || 5000;
 
